a83de14189d756377feb7ee05c90596410faa8e3
TheBrokenRail <connor24nolan@live.com>
2017-01-22 17:33:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var nativefier = require("nativefier").default;
+
+var options = {
+    name: 'Scratch 3.0', // will be inferred if not specified
+    targetUrl: 'llk.github.io/scratch-gui', // required
+    platform: 'win32', // defaults to the current system
+    arch: 'x64', // defaults to the current system
+    asar: true, // see conceal
+    showMenuBar: false,
+    min-width: 1200,
+    min-height: 800
+};
+nativefier(options, function(error, appPath) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log('App has been nativefied to', appPath);
+});
