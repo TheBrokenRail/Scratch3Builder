32b988e3531be8f5211d07cf63d882c1280ff3db
TheBrokenRail <connor24nolan@live.com>
2017-01-22 18:17:46 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  ./compile.sh
+  node index.js
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
