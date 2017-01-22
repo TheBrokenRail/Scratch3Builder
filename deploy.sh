22e40a035118c08de36f1efa89a55dc2fa2f4e59
TheBrokenRail <connor24nolan@live.com>
2017-01-22 18:54:31 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
+    echo "Pre-compiled"
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
