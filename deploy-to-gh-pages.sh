82a3670197f47cf21c95307040b421d767b80a79
TheBrokenRail <connor24nolan@live.com>
2017-01-22 19:47:16 -0500

    Update deploy-to-gh-pages.sh

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -17,4 +17,4 @@ cd out
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages
