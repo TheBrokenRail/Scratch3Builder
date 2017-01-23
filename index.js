194a6335adb4364bdde1e9b2e6152574f36db8ff
TheBrokenRail <connor24nolan@live.com>
2017-01-23 14:31:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -105,24 +105,3 @@ nativefier(options, function(error, appPath) {
     }
     console.log('App has been nativefied to', appPath);
 });
-options = {
-    name: 'Scratch 3.0', // will be inferred if not specified
-    targetUrl: 'llk.github.io/scratch-gui', // required
-    platform: 'mac', // defaults to the current system
-    arch: 'ia32', // defaults to the current system
-    asar: true, // see conceal
-    showMenuBar: false,
-    minWidth: 1200,
-    minHeight: 800,
-    width: 1200,
-    height: 800,
-    disableContextMenu: true,
-    out: './out'
-};
-nativefier(options, function(error, appPath) {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    console.log('App has been nativefied to', appPath);
-});
