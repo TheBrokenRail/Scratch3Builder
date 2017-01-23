63a8bc6ae6fa68963b49769fb242ecb7e1c52cc6
TheBrokenRail <connor24nolan@live.com>
2017-01-22 20:20:50 -0500

    Update deploy-to-gh-pages.sh

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -14,8 +14,16 @@ node index.js
 
 # deploy
 cd out
-zip -r Win32.zip "Scratch 3.0-win32-x64"
+zip -r Win32-x64.zip "Scratch 3.0-win32-x64"
 rm -r "Scratch 3.0-win32-x64"
+zip -r Win32-ia32.zip "Scratch 3.0-win32-ia32"
+rm -r "Scratch 3.0-win32-ia32"
+zip -r Linux-x64.zip "Scratch 3.0-linux-x64"
+rm -r "Scratch 3.0-linux-x64"
+zip -r Linux-ia32.zip "Scratch 3.0-linux-ia32"
+rm -r "Scratch 3.0-linux-ia32"
+zip -r Mac-x64.zip "Scratch 3.0-darwin-x64"
+rm -r "Scratch 3.0-darwin-x64"
 git init
 git add .
 git commit -m "Deploy to Github Pages"
