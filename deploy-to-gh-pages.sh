4b983cb0f212010ba36a31c0e6ac937a81df3976
TheBrokenRail <connor24nolan@live.com>
2017-01-22 20:43:12 -0500

    Update deploy-to-gh-pages.sh

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -22,8 +22,8 @@ zip -r Linux-x64.zip "Scratch 3.0-linux-x64"
 rm -r "Scratch 3.0-linux-x64"
 zip -r Linux-ia32.zip "Scratch 3.0-linux-ia32"
 rm -r "Scratch 3.0-linux-ia32"
-rm -f Index.md
-cp ../Index.md ./
+rm -f index.md
+cp ../index.md ./
 git init
 git add .
 git commit -m "Deploy to Github Pages"
