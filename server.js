0798370c82ef2a783c4a8a436e014a049bd48aac
TheBrokenRail <connor24nolan@live.com>
2017-01-25 18:47:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var path = require('path')
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(path.join(__dirname, 'editor'))).listen(8080, function(){
-    console.log('Server running on 8080...');
-});
+try {
+    var path = require('path')
+    var connect = require('connect');
+    var serveStatic = require('serve-static');
+    connect().use(serveStatic(path.join(__dirname, 'editor'))).listen(8080, function(){
+        console.log('Server running on 8080...');
+    });
+}
+catch {
+}
