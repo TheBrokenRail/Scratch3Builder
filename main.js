73f7df4a4e1c08823cd468daddf268b65237b04e
TheBrokenRail <connor24nolan@live.com>
2017-01-25 19:59:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -60,5 +60,6 @@ app.on('activate', function () {
   }
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+process.on('uncaughtException', function (e) {
+  console.log(e.Error)
+})
