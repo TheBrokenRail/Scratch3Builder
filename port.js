7e3154daa94d1b8ec51b773f24da715796f075a2
TheBrokenRail <connor24nolan@live.com>
2017-04-06 13:13:55 -0400

    Update port.js

--- port.js
+++ port.js
@@ -1,3 +1,12 @@
-var portFinderSync = require('portfinder-sync');
+var op = require('openport');
 
-module.exports = portFinderSync.getPort();
+module.exports = function (callback) {
+  op.find(function(err, port) {
+    if (err) {
+      console.log(err);
+      callback(8080);
+      return;
+    }
+    callback(port);
+  });
+}
