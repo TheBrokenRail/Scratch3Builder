a8feb469e502c238ec8e364b12c9af2f251c4aa0
TheBrokenRail <connor24nolan@live.com>
2017-04-06 13:16:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var path = require('path');
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var port = require('./port.js');
-module.exports = port;
-var server = connect().use(serveStatic(path.join(__dirname, 'editor')));
-server.listen(port, function(){
-  console.log('Server running on ' + port + '...');
-});
+var getPort = require('./port.js');
+module.exports = function (callback) {
+  getPort(function (port) {
+    var server = connect().use(serveStatic(path.join(__dirname, 'editor')));
+    server.listen(port, function(){
+      console.log('Server running on ' + port + '...');
+    });
+    callback(port);
+  });
+};
