fc1edaf422ccb3d2bb4b4e0b16a288a0a61218bb
TheBrokenRail <connor24nolan@live.com>
2017-04-26 07:23:21 -0400

    Update deploy-to-gh-pages.sh

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -45,6 +45,7 @@ rm -r "Scratch 3.0-darwin-x64"
 cd ../
 
 rm -r -f "editor"
+mkdir editor
 cd editor
 mkdir scratch-gui
 git clone https://github.com/LLK/scratch-gui.git scratch-gui-source
@@ -55,7 +56,29 @@ rename ./node_modules/scratch-blocks/dist/horizontal.js ./node_modules/scratch-b
 npm run build > /dev/null 2>&1
 cp -r ./build/* ../
 cd ../
+
+cd ../
+./node_modules/.bin/electron-packager . "Scratch 3.0" --platform win32 --arch x64 --out out --icon icon.ico --asar true
+./node_modules/.bin/electron-packager . "Scratch 3.0" --platform win32 --arch ia32 --out out --icon icon.ico --asar true
+./node_modules/.bin/electron-packager . "Scratch 3.0" --platform linux --arch x64 --out out --icon icon.png --asar true
+./node_modules/.bin/electron-packager . "Scratch 3.0" --platform linux --arch ia32 --out out --icon icon.png --asar true
+./node_modules/.bin/electron-packager . "Scratch 3.0" --platform linux --arch armv7l --out out --icon icon.png --asar true
+./node_modules/.bin/electron-packager . "Scratch 3.0" --platform darwin --arch x64 --out out --icon icon.icns --asar true
+
+# deploy
 cd out
+zip -r -q -9 Horizontal-Win32-x64.zip "Scratch 3.0-win32-x64"
+rm -r "Scratch 3.0-win32-x64"
+zip -r -q -9 Horizontal-Win32-ia32.zip "Scratch 3.0-win32-ia32"
+rm -r "Scratch 3.0-win32-ia32"
+tar -cvzf Horizontal-Linux-x64.tar.gz "Scratch 3.0-linux-x64" > /dev/null 2>&1
+rm -r "Scratch 3.0-linux-x64"
+tar -cvzf Horizontal-Linux-ia32.tar.gz "Scratch 3.0-linux-ia32" > /dev/null 2>&1
+rm -r "Scratch 3.0-linux-ia32"
+tar -cvzf Horizontal-Linux-armv7l.tar.gz "Scratch 3.0-linux-armv7l" > /dev/null 2>&1
+rm -r "Scratch 3.0-linux-armv7l"
+tar -cvzf Horizontal-Mac-x64.tar.gz "Scratch 3.0-darwin-x64" > /dev/null 2>&1
+rm -r "Scratch 3.0-darwin-x64"
 
 rm -r -f "scratch-gui-source"
 
